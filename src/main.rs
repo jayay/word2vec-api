c877844ef911161bfba3a85492c27a7b8e08c6a4
Jakob Jungmann <jungmann0@gmail.com>
2022-07-05 20:52:27 +0200

    upgrade to rocket 0.5-rc

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,11 @@ extern crate word2vec;
 extern crate word2vec_api_lib;
 use word2vec_api_lib::build_rocket;
 use std::env;
+use rocket::launch;
 
-fn main() {
+#[launch]
+fn rocket() -> _ {
     let args: Vec<String> = env::args().collect();
     let filename = args.get(1).expect("No file name given.");
-    build_rocket(filename).launch();
+    build_rocket(filename)
 }
--- tests/test_rocket_routes.rs
+++ tests/test_rocket_routes.rs
@@ -4,150 +4,148 @@ mod test {
     extern crate word2vec_api_lib;
     extern crate serde_json;
     extern crate serde;
-    
-    use rocket::local::Client;
+
+    use rocket::local::asynchronous::{Client};
     use rocket::http::{Status, ContentType};
     use word2vec_api_lib::*;
     use serde::Deserialize;
 
-    #[test]
-    fn test_index() {
-        let rocket = get_rocket_client();
+    #[tokio::test]
+    async fn test_index() {
+        let rocket = get_rocket_client().await;
         let request = rocket.get("/");
-        let mut response = request.dispatch();
+        let response = request.dispatch().await;
         assert_eq!(Status::SeeOther, response.status());
         assert_eq!(None, response.content_type());
-        assert_eq!(None, response.body_string());
+        assert!(response.body().is_none());
     }
 
-
-    #[test]
-    fn test_help() {
-        let rocket = get_rocket_client();
+    #[tokio::test]
+    async fn test_help() {
+        let rocket = get_rocket_client().await;
         let request = rocket.get("/help");
-        let mut response = request.dispatch();
+        let response = request.dispatch().await;
+
         assert_eq!(Status::Ok, response.status());
         assert_eq!(Some(ContentType::JSON), response.content_type());
-        assert_eq!(Some(r###"["GET /","GET /word_count","GET /vector_size","GET /vector/<word>","GET /cosine/<word>?<n>","GET /analogy?<pos>&<neg>&<n>","GET /show/me/to/<target>/what/<comparison>/is/to/<origin>","GET /help"]"###.to_string()), response.body_string());
+
+        assert_eq!(r###"["GET /","GET /word_count","GET /vector_size","GET /vector/<word>","GET /cosine/<word>?<n>","GET /analogy?<pos>&<neg>&<n>","GET /show/me/to/<target>/what/<comparison>/is/to/<origin>","GET /help"]"###.to_string(), response.into_string().await.unwrap());
     }
 
-    #[test]
-    fn test_word_count() {
-        let rocket = get_rocket_client();
+    #[tokio::test]
+    async fn test_word_count() {
+        let rocket = get_rocket_client().await;
         let request = rocket.get("/word_count");
-        let mut response = request.dispatch();
+        let response = request.dispatch().await;
         assert_eq!(Status::Ok, response.status());
         assert_eq!(Some(ContentType::JSON), response.content_type());
-        assert_eq!(Some("86588".to_string()), response.body_string());
+        assert_eq!("86588".to_string(), response.into_string().await.unwrap());
     }
 
 
-    #[test]
-    fn test_vector_size() {
-        let rocket = get_rocket_client();
+    #[tokio::test]
+    async fn test_vector_size() {
+        let rocket = get_rocket_client().await;
         let request = rocket.get("/vector_size");
-        let mut response = request.dispatch();
+        let response = request.dispatch().await;
         assert_eq!(Status::Ok, response.status());
         assert_eq!(Some(ContentType::JSON), response.content_type());
-        assert_eq!(Some("100".to_string()), response.body_string());
+        assert_eq!("100".to_string(), response.into_string().await.unwrap());
     }
 
 
-     #[test]
-    fn test_404() {
-        let rocket = get_rocket_client();
+     #[tokio::test]
+    async fn test_404() {
+        let rocket = get_rocket_client().await;
         let request = rocket.get("/a");
-        let mut response = request.dispatch();
+        let response = request.dispatch().await;
         assert_eq!(Status::NotFound, response.status());
         assert_eq!(Some(ContentType::JSON), response.content_type());
-        assert_eq!(Some(r###""Error 404, '/a' not found. See /help.""###.to_string()), response.body_string());
+        assert_eq!(r###""Error 404, '/a' not found. See /help.""###.to_string(), response.into_string().await.unwrap());
     }
 
-    #[test]
-    fn test_vector() {
-        let rocket = get_rocket_client();
+    #[tokio::test]
+    async fn test_vector() {
+        let rocket = get_rocket_client().await;
         let request = rocket.get("/vector/rust");
-        let mut response = request.dispatch();
+        let response = request.dispatch().await;
         assert_eq!(Status::Ok, response.status());
         assert_eq!(Some(ContentType::JSON), response.content_type());
-        let response_vec = serde_json::from_str::<Vec<f32>>(&response.body_string().unwrap()).unwrap();
+        let response_vec = serde_json::from_str::<Vec<f32>>(&response.into_string().await.unwrap()).unwrap();
         assert_eq!(100, response_vec.len());
         let average = response_vec.iter().sum::<f32>() as f32 / response_vec.len() as f32;
-        assert!(average != 0.0);
+        assert_ne!(average, 0.0);
     }
-
 
     #[derive(Deserialize, Debug, PartialEq)]
     struct Word2VecResult(String, f32);
 
-    #[test]
-    fn test_cosine_default() {
-        let rocket = get_rocket_client();
+    #[tokio::test]
+    async fn test_cosine_default() {
+        let rocket = get_rocket_client().await;
         let request = rocket.get("/cosine/computer");
-        let mut response = request.dispatch();
+        let response = request.dispatch().await;
         assert_eq!(Status::Ok, response.status());
         assert_eq!(Some(ContentType::JSON), response.content_type());
-        let response_vec = serde_json::from_str::<Vec<Word2VecResult>>(&response.body_string().unwrap()).unwrap();
+        let response_vec = serde_json::from_str::<Vec<Word2VecResult>>(&response.into_string().await.unwrap()).unwrap();
         assert_eq!(10, response_vec.len());
     }
 
-    #[test]
-    fn test_cosine_n() {
-        let rocket = get_rocket_client();
+    #[tokio::test]
+    async fn test_cosine_n() {
+        let rocket = get_rocket_client().await;
         let request = rocket.get("/cosine/software?n=3");
-        let mut response = request.dispatch();
+        let response = request.dispatch().await;
         assert_eq!(Status::Ok, response.status());
         assert_eq!(Some(ContentType::JSON), response.content_type());
-        let response_vec = serde_json::from_str::<Vec<Word2VecResult>>(&response.body_string().unwrap()).unwrap();
+        let response_vec = serde_json::from_str::<Vec<Word2VecResult>>(&response.into_string().await.unwrap()).unwrap();
         assert_eq!(3, response_vec.len());
 
         let expected_result = vec![
-            Word2VecResult("hardware".to_string(), 0.7883262634277344),
-            Word2VecResult("compiler".to_string(), 0.7580281496047974),
-            Word2VecResult("multitasking".to_string(), 0.7403385639190674),
+            Word2VecResult("hardware".to_string(), 0.788_326_26),
+            Word2VecResult("compiler".to_string(), 0.758_028_15),
+            Word2VecResult("multitasking".to_string(), 0.740_338_56),
         ];
         assert_eq!(expected_result, response_vec);
     }
 
-    #[test]
-    fn test_analogy_n() {
-        let rocket = get_rocket_client();
+    #[tokio::test]
+    async fn test_analogy_n() {
+        let rocket = get_rocket_client().await;
         let request = rocket.get("/analogy/?pos=woman+king&neg=man&n=1");
-        let mut response = request.dispatch();
-        check_analogy_1(&mut response);
-    }
+        let response = request.dispatch().await;
 
-
-    fn check_analogy_1(response: &mut rocket::Response) {
         assert_eq!(Status::Ok, response.status());
         assert_eq!(Some(ContentType::JSON), response.content_type());
-        let response_vec = serde_json::from_str::<Vec<Word2VecResult>>(&response.body_string().unwrap()).unwrap();
+
+        let body = &response.into_string().await.unwrap();
+        let response_vec: Vec<Word2VecResult> = serde_json::from_str(body).unwrap();
         assert_eq!(1, response_vec.len());
         assert_eq!("queen".to_string(), response_vec[0].0);
-        assert!((0.24765503406524658 - response_vec[0].1).abs() < 0.00001);
+        assert!((0.247_655_03 - response_vec[0].1).abs() < 0.00001);
     }
 
-    #[test]
-    fn test_analogy_default() {
-        let rocket = get_rocket_client();
+    #[tokio::test]
+    async fn test_analogy_default() {
+        let rocket = get_rocket_client().await;
         let request = rocket.get("/analogy/?pos=human+meat&neg=dog");
-        let mut response = request.dispatch();
+        let response = request.dispatch().await;
         assert_eq!(Status::Ok, response.status());
         assert_eq!(Some(ContentType::JSON), response.content_type());
-        let response_vec = serde_json::from_str::<Vec<Word2VecResult>>(&response.body_string().unwrap()).unwrap();
+        let response_vec = serde_json::from_str::<Vec<Word2VecResult>>(&response.into_string().await.unwrap()).unwrap();
         assert_eq!(10, response_vec.len());
 
         let expected_result = vec![
-            Word2VecResult("food".to_string(), 0.28690820932388306),
-            Word2VecResult("agricultural".to_string(), 0.28199446201324463),
-            Word2VecResult("cultivation".to_string(), 0.27436479926109314),
-            Word2VecResult("nutrition".to_string(), 0.27405092120170593),
-            Word2VecResult("agriculture".to_string(), 0.2718198299407959),
-            Word2VecResult("nutritional".to_string(), 0.26885557174682617),
-            Word2VecResult("subsistence".to_string(), 0.2645898461341858),
-            Word2VecResult("dairy".to_string(), 0.2601871192455292),
-            Word2VecResult("aspartame".to_string(), 0.2572169601917267),
-            Word2VecResult("dietary".to_string(), 0.2545112371444702),
+            Word2VecResult("food".to_string(), 0.286_908_2),
+            Word2VecResult("agricultural".to_string(), 0.281_994_46),
+            Word2VecResult("cultivation".to_string(), 0.274_364_8),
+            Word2VecResult("nutrition".to_string(), 0.274_050_92),
+            Word2VecResult("agriculture".to_string(), 0.271_819_83),
+            Word2VecResult("nutritional".to_string(), 0.268_855_57),
+            Word2VecResult("subsistence".to_string(), 0.264_589_85),
+            Word2VecResult("dairy".to_string(), 0.260_187_12),
+            Word2VecResult("aspartame".to_string(), 0.257_216_96),
+            Word2VecResult("dietary".to_string(), 0.254_511_24),
         ];
 
         for (index, vec_result) in expected_result.iter().enumerate() {
@@ -157,33 +155,40 @@ mod test {
     }
 
 
-     #[test]
-    fn test_analogy_nice_default() {
-        let rocket = get_rocket_client();
+     #[tokio::test]
+    async fn test_analogy_nice_default() {
+        let rocket = get_rocket_client().await;
         let request = rocket.get("/show/me/to/woman/what/king/is/to/man/");
-        let mut response = request.dispatch();
-        check_analogy_1(&mut response);
+        let response = request.dispatch().await;
+        assert_eq!(Status::Ok, response.status());
+        assert_eq!(Some(ContentType::JSON), response.content_type());
+
+        let body = response.into_string().await.unwrap();
+        // let mut clone = &mut body.to_bytes().await.unwrap();
+        let response_vec: Vec<Word2VecResult> = serde_json::from_str(&body).unwrap();
+        assert_eq!(1, response_vec.len());
+        assert_eq!("queen".to_string(), response_vec[0].0);
+        assert!((0.247_655_03 - response_vec[0].1).abs() < 0.00001);
     }
 
 
-    #[test]
+    #[tokio::test]
     #[should_panic]
-    fn test_build_rocket_invalid_filename() {
-        build_rocket("unknown");
+    async fn test_build_rocket_invalid_filename() {
+        let _ = build_rocket("unknown");
     }
 
 
-    #[test]
+    #[tokio::test]
     #[should_panic]
-    fn test_build_rocket_invalid_filecontents() {
-        build_rocket("Cargo.toml");
+    async fn test_build_rocket_invalid_filecontents() {
+        let _ = build_rocket("Cargo.toml");
     }
 
 
-    fn get_rocket_client() -> Client {
+    async fn get_rocket_client() -> Client {
         let filename = "tests/model/trained-small.bin";
         let rocket = build_rocket(filename);
-        let client = Client::new(rocket).expect("valid rocket instance");
-        client
+        Client::untracked(rocket).await.expect("valid rocket instance")
     }
 }
